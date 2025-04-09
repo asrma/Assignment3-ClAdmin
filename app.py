dac30ad42bfe178aafc9bc46669d0678d535ad1b
Ashutosh Sharma <asrma.sharma@gmail.com>
2025-04-09 18:51:53 -0400

    dev update

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask('app')
 
 @app.route('/')
 def hello_world():
-    return 'Hello from Sharma ECS Container.'
+    return 'Hello from Sharma ECS Container. (Updated from dev branch)'
 
 
 app.run(host='0.0.0.0')
